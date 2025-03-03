da222b4e87ae30492816e65cc62318c0a6d4dc70
utkrshm <malaiya.utkarsh@gmail.com>
2025-03-04 00:24:06 +0530

    Basic tool calling chatbot

--- task 2/main.py
+++ task 2/main.py
@@ -5,17 +5,19 @@ from pydantic import BaseModel, Field
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.tools import tool
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import HumanMessage
 
 # Initializing all API Keys [Google, Tavily, Pinecone]
 init_keys()
 
+
+# Creating tool for Arithmetic Input
 class ArithmeticInput(BaseModel):
     operation: str = Field(
         description="The operation that needs to be performed.",
         examples=["2+3", "(3+4)-(2+4)"]
     )
 
-# Use the schema with your tool
 @tool(args_schema=ArithmeticInput)
 def arithmetic(operation: str):
     """Performs arithmetic operation
@@ -31,10 +33,27 @@ def arithmetic(operation: str):
     except Exception as e:
         return f"Error evaluating expression: {str(e)}"
 
-
-search = TavilySearchResults(max_results=2, include_images=False)
+# Creating tool for Searching
+search = TavilySearchResults(max_results=2, include_images=False, name='tavily_search')
 
 llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash', api_key=os.environ['GOOGLE_API_KEY'])
 llm_with_tools = llm.bind_tools([arithmetic, search])
 
-print(llm_with_tools.invoke(input="Who won the last cricket match between India and New Zealand?").tool_calls)
+messages = []
+
+for i in range(3):
+    msg = input("Human: ")
+    messages.append(HumanMessage(msg))
+    
+    ai_msg = llm_with_tools.invoke(msg)
+    
+    messages.append(ai_msg)
+    
+    for tool_call in ai_msg.tool_calls:
+        selected_tool = {'arithmetic': arithmetic, "tavily_search": search}[tool_call['name'].lower()]
+        tool_msg = selected_tool.invoke(tool_call)
+        messages.append(tool_msg)
+    
+    print("AI: " + llm_with_tools.invoke(messages).content)
+
+print(messages)
