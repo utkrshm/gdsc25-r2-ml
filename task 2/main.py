d18c845de9c042db2ab056c155f27283abfaacee
utkrshm <malaiya.utkarsh@gmail.com>
2025-03-03 23:51:39 +0530

    Initial trial of LLM

--- task 2/main.py
+++ task 2/main.py
@@ -1,5 +1,23 @@
-import langchain
+from apikeys import init_keys
 
+import os
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_community.tools import tool
+from langchain_community.tools.tavily_search import TavilySearchResults
 
-llm = 
+# Initializing all API Keys
+init_keys()
+
+@tool
+def arithmetic(operation):
+    """Performs arithmetic operation
+
+    Args:
+        operation (str): The operation that needs to be performed.
+            Example. If 2 and 3 are to be added, then operation is "2+3"
+                     If 2 and 4 are to be subtracted from the sum of 3 + 4, then operation is "(3+4)-(2+4)"
+    """
+    return eval(operation) 
+
+llm = ChatGoogleGenerativeAI(model='gemini-2.0-flash', api_key=os.environ['GOOGLE_API_KEY'])
+print(llm.invoke("What is the capital of India?"))
